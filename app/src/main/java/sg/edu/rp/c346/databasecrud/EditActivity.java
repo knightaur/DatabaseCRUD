7ae99cf0bb377de2f0295a9e5d0fde189d7a9fce
knightaur <francisanthonytoha@gmail.com>
2019-05-21 12:26:59 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/databasecrud/EditActivity.java
@@ -0,0 +1,64 @@
+package sg.edu.rp.c346.databasecrud;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EditActivity extends AppCompatActivity {
+
+    TextView tvID;
+    EditText etContent;
+    Button btnUpdate, btnDelete;
+    Note data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        //initialize the variables with UI here
+        btnDelete = findViewById(R.id.btnDelete);
+        etContent = findViewById(R.id.etContent);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        tvID = findViewById(R.id.tvID);
+
+        Intent i = getIntent();
+        data = (Note) i.getSerializableExtra("data");
+
+        tvID.setText("ID: " + data.getId());
+        etContent.setText(data.getNoteContent());
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                data.setNoteContent(etContent.getText().toString());
+                dbh.updateNote(data);
+                dbh.close();
+                Intent returnIntent = new Intent();
+                setResult(MainActivity.RESULT_OK,returnIntent);
+                finish();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                dbh.deleteNote(data.getId());
+                dbh.close();
+                Intent returnIntent = new Intent();
+                setResult(MainActivity.RESULT_OK,returnIntent);
+                finish();
+            }
+        });
+
+
+
+    }
+}
+
